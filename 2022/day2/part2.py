3c60dff4991a163310e4b0d5e8f664fb0fdc0b5c
t-88 <temptaki5002@gmail.com>
2023-06-04 18:48:38 +0100

    readme and license

--- 2022/day2/part2.py
+++ 2022/day2/part2.py
@@ -13,7 +13,7 @@ def round_score(round):
     return  score + (2 if round[0] == 0 else round[0] - 1) 
 
 total_sum = 0
-with open("input.txt","r") as f:
+with open(" input.txt","r") as f:
     for line in f:
         round = line.strip().split(" ")
         total_sum += round_score(round) + 1 
---
+++ README.md
@@ -0,0 +1,2 @@
+# Advet of Code 2022
+trying out advent of code for the first time
