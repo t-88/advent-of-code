768105b00c27d346d8e4591ea860e6db8b78260c
tea <tea@no-email.com>
2023-06-08 19:36:30 +0100

    fixing loop

--- 2022/filified.py
+++ 2022/filified.py
@@ -1,5 +1,6 @@
 def file_to_arr(fileName,func=lambda x : x ):
     out = [] 
     with open(fileName,"r") as f:
-        out.append(func(f.readline()))
+        for line in f:
+            out.append(func(line))
     return out
